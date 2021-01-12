371a618f15394f78925e6ed3adec77818f74bb67
Alex Taylor <altays633@gmail.com>
2021-01-12 15:28:45 -0500

    adding function that pulls text from database, compares against analyzed text, and returns unique words

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -9,6 +9,14 @@ let sentenceObjArray = []
 let cleanObjArray = []
 let cleanSentenceArray = []
 
+// Connection URL, db name
+const url = 'mongodb://localhost:27017';
+const dbName = 'randomText';
+const rawColWords = 'rawWords'
+const rawColPatterns = 'rawPatterns'
+const cleanColWords = 'cleanWords'
+const cleanColPatterns = 'cleanPatterns' 
+
 // reading test file
 fs.readFile('./testText.txt', 'utf8', async function(err, contents) {
     try {
@@ -68,128 +76,51 @@ fs.readFile('./testText.txt', 'utf8', async function(err, contents) {
             let posNLP = Object.values(sentencesTags[index]); 
             helpFunc.aggreObjects("pattern", JSON.stringify(posNLP), sentenceObjArray)
         }
-        console.log("done reading file")
+        let newWordsArray = []
+        MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
+            const db = client.db(dbName);
+           
+            db.collection(rawColWords).find({}).toArray( async function(err, docs) {
+                try {
+                    const dbWordsToArray = async() => {
+                        let dbWordList = []
+                        for (let index = 0; index < docs.length; index++) {
+                            const doc = docs[index].word
+                            dbWordList.push(doc)
+                        }
+                        return dbWordList
+                    }
+                   
+                    let dbWords = await dbWordsToArray()
+                    
+                    const compareDbToRaw = async() => {
+                        let newArray = []
+                        for (let rawIndex = 0; rawIndex < wordObjArray.length; rawIndex++){
+                            index = dbWords.indexOf(wordObjArray[rawIndex].word)
+                            if (index == -1) {
+                                await newArray.push(wordObjArray[rawIndex])
+                            }
+                        }
+                        return newArray
+                    }
+            
+                    newWordsArray = await compareDbToRaw()
+                    await console.log(`Length of first array: ${wordObjArray.length}`)
+                    await console.log(`Length of db array: ${dbWords.length}`)
+                    await console.log(`Length of new array: ${newWordsArray.length}`)
+                    await client.close(function() {
+                        console.log("close connection")
+                    })
+                } catch {
+                    console.log(err)
+                    await client.close(function() {
+                        console.log("close connection")
+                    })
+                }
+            });
+        });
     }
     catch {
         console.log(err)
     }
 });
-
-// Connection URL, db name
-const url = 'mongodb://localhost:27017';
-const dbName = 'randomText';
-const rawColWords = 'rawWords'
-const rawColPatterns = 'rawPatterns'
-const cleanColWords = 'cleanWords'
-const cleanColPatterns = 'cleanPatterns' 
-
-
-// MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
-//     const db = client.db(dbName);
-//     db.collection(rawColWords).find({}).toArray( async function(err, docs) {
-//         try {
-//             await console.log("Found the following records");
-//             // await console.log(docs);
-//             dbWords = docs
-        
-//             client.close(function() {
-//                 console.log("close connection")
-//             })
-//         } catch {
-//             console.log(err)
-//         }
-//     });
-// });
-
-MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
-    const db = client.db(dbName);
-    results = db.collection(rawColWords).find({}).toArray( async function(err, docs) {
-        try {
-            const dbWordsToArray = async() => {
-                let dbWordList = ""
-                for (let index = 0; index < docs.length; index++) {
-                    const doc = docs[index].word
-                    dbWordList += doc + " ";
-                }
-                return dbWordList.split(" ")
-            }
-           
-            let dbWords = await dbWordsToArray()
-
-            const compareDbToRaw = async() => {
-                // use a loop
-                // using findIndex()
-            }
-            // make another async function to compare the returned words?
-            // 
-
-
-            await console.log(dbWords)
-            await client.close(function() {
-                console.log("close connection")
-            })
-        } catch {
-            console.log(err)
-        }
-    });
-});
-
-
-// MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
-//     console.log("Connected successfully to raw word collection");
-//     const db = client.db(dbName);
-//     try {
-//         db.collection(rawColWords).insertMany(wordObjArray)
-//         .then( async function(result,err) {
-//             console.log(result)
-//             client.close(function() {
-//                 console.log("close connection")
-//             })
-//         })
-//     }
-//     catch {
-//         console.error(err)
-//     }
-// });
-
-// MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
-//     console.log("Connected successfully to raw pattern collection");
-//     const db = client.db(dbName);
-//     try {
-//         db.collection(rawColPatterns).insertMany(sentenceObjArray)
-//         .then( async function(result,err) {
-//             console.log(result)
-//             client.close(function() {
-//                 console.log("close connection")
-//             })
-//         })
-//     }
-//     catch {
-//         console.error(err)
-//     }
-// });
-
-// MongoClient.connect(url,  { useUnifiedTopology: true }, async function(err, client) {
-//     const db = client.db(dbName);
-//     db.collection(rawColWords).find({}).toArray( async function(err, docs) {
-//         try {
-//             await console.log("Found the following records");
-//             await console.log(docs);
-        
-//             client.close(function() {
-//                 console.log("close connection")
-//             })
-//         } catch {
-//             console.log(err)
-//         }
-//     });
-// });
-
-// use this query
-    // db.getCollection('rawWords').find({word:{'$exists': true}}).count()
-    // if count is greater than one, remove by the ID
-// start with a call to database
-    // put keys into an array
-    // check pulled words against database array - if the pulled word is not in the database, insert it
-
-// close database
